557852e1c23af610d442785e31b9a1da11bf88c3
UO265181 <UO265181@DESKTOP-M5AP9DI>
2021-03-10 04:39:49 +0100

    SDI-2021-105-5.4-Casos Uso VistaHome.

--- sdi2021-105-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi2021-105-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -5,58 +5,76 @@ import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+
 import org.junit.runners.MethodSorters;
+
 //Ordenamos las pruebas por el nombre del método
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
-
 public class NotaneitorTests {
-	
-	
-	
-	//En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens automáticas)):
+
+	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
+	// automáticas)):
 	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
 	static String Geckdriver024 = "D:\\Temporal\\_CLASE\\SDI\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
-	
-	
-	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024); 
+
+	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
 	static String URL = "http://localhost:8090";
+
 	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
-	System.setProperty("webdriver.firefox.bin", PathFirefox);
-	System.setProperty("webdriver.gecko.driver", Geckdriver);
-	WebDriver driver = new FirefoxDriver();
-	return driver;
-	} 
-	
-	
-	
-	
-	
-	
-	//Antes de cada prueba se navega al URL home de la aplicación
+		System.setProperty("webdriver.firefox.bin", PathFirefox);
+		System.setProperty("webdriver.gecko.driver", Geckdriver);
+		WebDriver driver = new FirefoxDriver();
+		return driver;
+	}
+
+	// Antes de cada prueba se navega al URL home de la aplicación
 	@Before
-	public void setUp(){
-	driver.navigate().to(URL);
+	public void setUp() {
+		driver.navigate().to(URL);
 	}
-	//Después de cada prueba se borran las cookies del navegador
+
+	// Después de cada prueba se borran las cookies del navegador
 	@After
-	public void tearDown(){
-	driver.manage().deleteAllCookies();
+	public void tearDown() {
+		driver.manage().deleteAllCookies();
 	}
-	//Antes de la primera prueba
+
+	// Antes de la primera prueba
 	@BeforeClass
 	static public void begin() {
 	}
-	//Al finalizar la última prueba 
+
+	// Al finalizar la última prueba
 	@AfterClass
 	static public void end() {
-	//Cerramos el navegador al finalizar las pruebas
-	driver.quit();
+		// Cerramos el navegador al finalizar las pruebas
+		driver.quit();
+	}
+
+	// PR01. Acceder a la página principal /
+	@Test
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
 	}
 	
-
+	//PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void PR02() {
+	PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+	//PR03. OPción de navegación. Pinchar en el enlace Identificate en la página home
+	@Test
+	public void PR03() {
+	PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+	//PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a Español
+	@Test
+	public void PR04() {
+	PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", 
+	PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+	//SeleniumUtils.esperarSegundos(driver, 2);
 	}
 }
